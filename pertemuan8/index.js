a9e4ea6712e236f05ad220f68ac2cf8e6bb0bf86
Muhamad Fauzi <114374732+MuhFauziIT@users.noreply.github.com>
2023-12-01 09:04:39 +0700

    Praktikum 8

---
+++ pertemuan8/index.js
@@ -0,0 +1,35 @@
+// Variable
+let name = "Muhamad Fauzi";
+console.log(name);
+
+// Array
+let angka = [1, 2, 3, 4, 5];
+console.log(angka[0]);
+console.log(angka.length);
+
+// Object
+let mahasiswa = {
+  nama: "Muhamad Fauzi",
+  nim: "0110222167",
+  jurusan: "TI",
+};
+console.log(mahasiswa.jurusan);
+
+// If Else
+let nilai = 80;
+if (nilai >= 80) {
+  console.log("Lulus");
+} else {
+  console.log("Tidak Lulus");
+}
+
+// Looping
+for (let i = 1; i < 10; i++) {
+  console.log(i);
+}
+
+// Function
+function tambah(a, b) {
+  return a + b;
+}
+console.log(tambah(1, 2));
