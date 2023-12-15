795430e4689067ef4b1c7188498c6e54b8bee139
Muhamad Fauzi <114374732+MuhFauziIT@users.noreply.github.com>
2023-12-15 10:52:01 +0700

    Delete pertemuan10 directory

--- pertemuan10/set-timeout-2.js
+++
@@ -1,9 +0,0 @@
-console.log("Fauzi membuka browser crome");
-
-setTimeout(()=>{
-    console.log("Mendownload file 1 jam");
-    console.log("Download Selesai");
-
-},5000);
-
-console.log("Fauzi membuka Youtube");
